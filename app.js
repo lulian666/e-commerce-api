f49e4474b11baad0e5965a628889d577b25561b4
lulian666 <644139490@qq.com>
2022-12-31 14:10:43 +0800

    product api & prettier

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
 
 const connectDB = require('./db/connect')
 require('dotenv').config()
@@ -10,6 +11,7 @@ require('express-async-errors')
 
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
+const productRouter = require('./routes/productRoutes')
 
 const notFound = require('./middleware/error-handler')
 const errorHandlerMiddleware = require('./middleware/not-found')
@@ -17,6 +19,10 @@ const errorHandlerMiddleware = require('./middleware/not-found')
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET))
+
+app.use(express.static('./public'))
+app.use(fileUpload())
+
 app.get('/hi', (req, res) => {
     console.log('req.cookies', req.signedCookies)
     res.send('Home Page')
@@ -24,6 +30,7 @@ app.get('/hi', (req, res) => {
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
+app.use('/api/v1/products', productRouter)
 
 app.use(notFound)
 app.use(errorHandlerMiddleware)
---
+++ routes/productRoutes.js
@@ -0,0 +1,31 @@
+const {
+    createProduct,
+    getAllProducts,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
+    uploadImage,
+} = require('../controllers/productController')
+
+const express = require('express')
+const router = express.Router()
+
+const {
+    authenticateUser,
+    authorizePermissions,
+} = require('../middleware/authentication')
+
+router
+    .route('/')
+    .post([authenticateUser, authorizePermissions('admin')], createProduct)
+    .get(getAllProducts)
+router
+    .route('/uploadImage')
+    .post([authenticateUser, authorizePermissions('admin')], uploadImage)
+router
+    .route('/:id')
+    .get(getSingleProduct)
+    .patch([authenticateUser, authorizePermissions('admin')], updateProduct)
+    .delete([authenticateUser, authorizePermissions('admin')], deleteProduct)
+
+module.exports = router
